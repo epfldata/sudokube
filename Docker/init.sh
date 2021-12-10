d789337406fb4d4980b482f73d1251f7b0881caa
Sachin Basil John <sachinbjohn@gmail.com>
2021-12-10 09:58:19 +0100

    add recommended cube params in schema

---
+++ Docker/init.sh
@@ -0,0 +1,24 @@
+apt update
+apt install -y vim \
+htop \
+bash \
+tar \
+wget \
+byobu \
+ca-certificates \
+curl \
+rsync \
+make \
+cmake  \
+build-essential \
+openjdk-8-jdk \
+unzip 
+
+
+wget https://github.com/sbt/sbt/releases/download/v1.2.7/sbt-1.2.7.zip
+unzip sbt-1.2.7.zip && mv sbt /opt/sbt && rm sbt-1.2.7.zip
+echo "export PATH=/opt/sbt/bin:$PATH" >> /root/.profile
+
+#make shared lib !
+
+
