c80792b4563da38c0523740035d092ca1b7c364e
Antonio06188 <antoine.metivier@epfl.ch>
2022-04-06 16:31:25 +0200

    modif Time schema and isNull Encoder

--- src/main/scala/frontend/schema/TimeSeriesSchema.scala
+++ src/main/scala/frontend/schema/TimeSeriesSchema.scala
@@ -31,39 +31,24 @@ class TimeSeriesSchema extends Schema {
       } else
         throw new UnsupportedOperationException("Only CSV or JSON supported")
     }
-    items.zip(Range(1, items.size+1)).map(l => (encode_tuple(List((columnTimeLabel, l._2)) ++ l._1.toList), 1L))
+    items.zip(Range(0, items.size)).map(l => (encode_tuple(List((columnTimeLabel, l._2)) ++ l._1.toList), 1L))
   }
 
     protected def encode_column(key: String, v: Any) = {
 
       //check if first column
         if(key == columnTimeLabel) {
-            if(v.isInstanceOf[Int]) {
-                val vi : Int = v.asInstanceOf[Int]
-                val sgn_enc = if(vi < 0) {
-                                    val sgn_key = "-" + key
-                                    val sgn_c = columns.getOrElse(key, new NatCol())
-                                    columns(sgn_key) = sgn_c
-                                    sgn_c.encode_any(1)
-                                } 
-                                else BigBinary(0)
-
-                val c = columns.getOrElse(key, new NatCol())
-                columns(key) = c
-                c.encode_any(math.abs(vi)) + sgn_enc
-            }
-            else {
-                val c = columns.getOrElse(key, new MemCol[Option[String]](List[Option[String]](None)))
-                columns(key) = c
-                c.encode_any(Some(v))
-            }
+          val vi : Int = v.asInstanceOf[Int]
+          val c = columns.getOrElse(key, new NatCol())
+          columns(key) = c
+          c.encode_any(vi)
         }
         else {
-           val c = if(v.isInstanceOf[Int]) {
-           columns.getOrElse(key, new IsNullColEncoder[Option[Int]])
-           } else columns.getOrElse(key, new IsNullColEncoder[Option[String]])
-            columns(key) = c
-            c.encode_any(Some(v))
+          val c = if(v.isInstanceOf[Int]) {
+          columns.getOrElse(key, new IsNullColEncoder[Option[Int]])
+          } else columns.getOrElse(key, new IsNullColEncoder[Option[String]])
+          columns(key) = c
+          c.encode_any(Some(v))
         }
 
     }
