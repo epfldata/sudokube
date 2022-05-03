f8b60d0b17c4491f6c765e6bf2cdd81dc6c1dc17
Eloi <eloi.eynard@hotmail.com>
2022-05-03 11:47:16 +0200

    added cheap_size check and functionnality in DAG prepare

--- src/main/scala/core/MaterializationScheme.scala
+++ src/main/scala/core/MaterializationScheme.scala
@@ -647,6 +647,7 @@ case class DAGMaterializationScheme(m: MaterializationScheme) extends Materializ
   override def prepare(query: Seq[Int], cheap_size: Int, max_fetch_dim: Int): List[ProjectionMetaData] = {
     val qIS = query.toIndexedSeq
     val qBS = query.toSet
+    val hm_cheap = collection.mutable.HashMap[Seq[Int], DagVertex]()
 
     val ret = new ListBuffer[ProjectionMetaData]()
     val queue = collection.mutable.Queue[(DagVertex, Seq[Int])]()
@@ -661,6 +662,28 @@ case class DAGMaterializationScheme(m: MaterializationScheme) extends Materializ
             queue.enqueue((child._1, intersect_deq.diff(child._2)))
           }
         })
+      } else if(vert_deq.p_length <= cheap_size){
+        //When we reach cheap size, is basically the same algorithm as prepare_new
+        var good_children = 0
+        //Still iterate through children since if one of the children has the same intersection then
+        //it will dominate => reduce further computation
+        vert_deq.children.foreach(child => {
+          val newdif = intersect_deq.intersect(child._2)
+          if (newdif.isEmpty && !child._1.hasBeenDone) {
+            queue.enqueue((child._1, intersect_deq))
+            child._1.hasBeenDone = true
+            good_children += 1
+          }
+        })
+        if (good_children == 0) {
+          val ab0 = intersect_deq
+          val res = hm_cheap.get(ab0)
+          if(res.isDefined){
+            if(vert_deq.p_length < res.get.p_length){
+              hm_cheap(ab0) = vert_deq
+            }
+          }
+        }
       } else {
         var good_children = 0
         vert_deq.children.foreach(child => {
@@ -686,6 +709,11 @@ case class DAGMaterializationScheme(m: MaterializationScheme) extends Materializ
         }
       }
     }
+    hm_cheap.foreach({ case (ab0, dv) =>
+      val ab = qIS.indices.filter(i => ab0.contains(qIS(i)))
+      val mask = Bits.mk_list_mask(dv.p.toList, qBS)
+      ret += ProjectionMetaData(ab, ab0, mask, dv.id)
+    })
     ret.toList
   }
 
