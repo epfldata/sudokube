8b73bd158f58477fdd6a23e395eaeae5c2a40f66
emonnin-epfl <etienne.monnin@epfl.ch>
2022-05-04 19:12:52 +0200

    .

--- src/main/scala/frontend/generators/JsonGenerator.scala
+++ src/main/scala/frontend/generators/JsonGenerator.scala
@@ -6,10 +6,11 @@ import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import com.fasterxml.jackson.module.scala.experimental.ScalaObjectMapper
 import java.io._
 import org.apache.commons.lang3.RandomStringUtils;
+import java.time.Year
 
 object JsonGenerator {
     def main(args: Array[String]): Unit = {
-        JsonWriter.gen("random.json", 2, List(SimpleField("date", DateGenerator()),SimpleField("email", EmailGenerator()), SimpleFieldInt("id", IntGenerator()), NestedJson("nested", List(SimpleField("name", NameGenerator())))))
+        JsonWriter.gen("random.json", 100, List(SimpleField("date", DateGenerator()),SimpleField("email", EmailGenerator()), SimpleFieldInt("id", IntGenerator()), NestedJson("nested", List(SimpleField("name", NameGenerator())))))
   }
 }
 
@@ -19,11 +20,11 @@ object JsonWriter {
 
         val mapper = new ObjectMapper() with ScalaObjectMapper
         mapper.registerModule(DefaultScalaModule)
-        val file = new File("country.json")
+        val file = new File(filename)
         val fileWriter = new FileWriter(file, true)
         val sequenceWriter = mapper.writerWithDefaultPrettyPrinter().writeValuesAsArray(fileWriter)
 
-        for(i <- 0 to length) {
+        for(i <- 0 to length - 1) {
              val map = scala.collection.mutable.Map[String,Any]()
             for (f <- schema) {
                 f match {
@@ -66,6 +67,13 @@ case class SimpleFieldDouble(key : String, value : MyGenerator[Double]) extends 
 case class SimpleFieldFloat(key : String, value : MyGenerator[Float]) extends Field
 case class NestedJson(key : String, value : List[Field]) extends Field
 
+
+object Order {
+    val YEAR_FIRST = 0
+    val DAY_FIRST = 1
+    val RANDOM = 2
+}
+
 abstract class MyGenerator[T]() {
     def generate() : T
 }
@@ -86,14 +94,14 @@ case class NameGenerator(length : Int = 4) extends MyGenerator[String] {
     }
 }
 
-case class DateGenerator(separetor : Char = '/') extends MyGenerator[String] {
+case class DateGenerator(separetor : Char = '/', yearBegin : Int = 1903, order : Int = Order.RANDOM) extends MyGenerator[String] {
     private val months = Map((1,31), (2,29), (3,31), (4, 30),(5,31), (6,30), (7,31), (8,31), (9,30), (10,31), (11, 31), (12,31))
 
     def generate() : String = {
-        val year = IntGenerator(1900,2021).generate()
+        val year = IntGenerator(1900,Year.now.getValue).generate()
         var month = IntGenerator(1,12).generate()
         var day = 0
-        if(month == 2 && year%4 == 0) {
+        if(month == 2 && year % 4 == 0) {
             day = IntGenerator(1,28).generate()
         }
         else {
@@ -112,11 +120,35 @@ case class DateGenerator(separetor : Char = '/') extends MyGenerator[String] {
         if(month < 10) {
             monthStr = "0" + month.toString()
         }
+
         else {
             monthStr = month.toString()
         }
+
+        var yearStr = ""
+        if(year < 10) {
+            yearStr = "0" + year.toString()
+        }
+        else {
+            yearStr = year.toString()
+        }
         
-       return year.toString() + separetor + monthStr + separetor + dayStr
+        if(order == Order.YEAR_FIRST) {
+              return yearStr + separetor + monthStr + separetor + dayStr
+        }
+        else if(order == Order.DAY_FIRST) {
+            return dayStr + separetor + monthStr + separetor + yearStr
+        }
+        else {
+            val rdm = Random.nextInt(2)
+
+            if(rdm == 0) {
+                return yearStr + separetor + monthStr + separetor + dayStr
+            }
+            else {
+                return dayStr + separetor + monthStr + separetor + yearStr
+            }
+        }
     }
 
 }
@@ -126,6 +158,8 @@ case class IntGenerator(start : Int = 1, end : Int = 10) extends MyGenerator[Int
         return start + Random.nextInt( (end - start) + 1 ) 
     }
 }
+
+
 
 
 
