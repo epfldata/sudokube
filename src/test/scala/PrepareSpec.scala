5ffbcf1c5f0e00cf8857f55867db357a2beeec63
Eloi <eloi.eynard@hotmail.com>
2022-05-10 16:44:00 +0200

    preparespec modified for testing on server

--- src/test/scala/PrepareSpec.scala
+++ src/test/scala/PrepareSpec.scala
@@ -10,12 +10,12 @@ class PrepareSpec extends FlatSpec with Matchers {
 
   def RMS(nbits: Int, dmin: Int, logncubs: Int, nq: Int, qs: Int, cheap: Int, maxFetch: Int): Unit = {
     val m = RandomizedMaterializationScheme2(nbits, logncubs, dmin + logncubs - 1, 0)
-    //val m_DAG = DAGMaterializationScheme(m)
+    val m_DAG = DAGMaterializationScheme(m)
     val m_new = testMaterializationScheme(m)
 
     (0 until nq).foreach{ i =>
       val q = Tools.rand_q(nbits, qs)
-      //val testp = Profiler("DagPrepare"){m_DAG.prepare(q, cheap, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
+      val testp = Profiler("DagPrepare"){m_DAG.prepare(q, cheap, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       val oldp = Profiler("OldPrepare"){m.prepare_old(q, cheap, maxFetch)}.map(p => ProjectionMetaData(p.accessible_bits, p.accessible_bits0.toList.sorted, p.mask, p.id)).sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       //val optp = Profiler("OptPrepare"){m.prepare_opt(q, cheap, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       val newp = Profiler("NewPrepare"){m.prepare_new(q, cheap, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
@@ -28,7 +28,7 @@ class PrepareSpec extends FlatSpec with Matchers {
     Profiler.print()
   }
 
-  "Old and New Prepare " should " match " in RMS(400, 15, 17, 100, 10, 40, 40)
+  "Old and New Prepare " should " match " in RMS(200, 15, 15, 100, 10, 40, 40)
   //"Old and New Prepare " should " match " in RMS(10, 2, 3, 100, 5, 10, 10)
 
 }
