c56e4bcbcc884b165ef8874b9d0d4e30b76c3413
Sachin Basil John <sachinbjohn@gmail.com>
2022-05-17 18:09:40 +0200

    Scripts for init

---
+++ scripts/setupRemote.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+nickname=$1
+hostname="iccluster$2"
+
+case "$nickname" in
+"kube1")
+  line=8
+  ;;
+"kube2")
+  line=15
+  ;;
+*)
+  echo "First argument must be kube1 or kube2"
+  exit
+esac
+
+sed -i.bu "${line}s/iccluster[0-9]*/$hostname/" ~/.ssh/config
+sedstr="sed -i.bu  '${line}s/iccluster[0-9]*/${hostname}/' ~/.ssh/config"
+echo $sedstr
+ssh-copy-id $nickname
+ssh-copy-id -i ~/.ssh/server_rsa.pub $nickname
+scp ./Docker/init.sh $nickname:.
+
+ssh $nickname "mkdir -p  /root/.ssh /var/data/sudokube/sudokube"
+
+ssh datadell "$sedstr"
+ssh datadell "scp ~/.rclone.conf $nickname:."
+ssh datadell "scp ~/sudokube/sudokube/.jvmopts $nickname:/var/data/sudokube/sudokube/"
+ssh datadell "scp ~/.ssh/id* $nickname:/root/.ssh"
+
+ssh $nickname 'chmod +x ./init.sh && ./init.sh'
+scripts/sync.sh $nickname
+ssh $nickname 'cd /var/data/sudokube && make'
+
+ssh datadell "~/sudokube/sudokube/scripts/uniqsyncto $nickname SSB/sf100"
+ssh datadell "~/sudokube/sudokube/scripts/uniqsyncto $nickname nyc"
+
