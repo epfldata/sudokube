6f60b2c30ee72cc102ae4f21b56e5d85579bab0c
Antonio06188 <antoine.metivier@epfl.ch>
2022-06-11 15:47:09 +0200

    Analyse

---
+++ src/main/scala/frontend/TestLine.scala
@@ -0,0 +1,144 @@
+package frontend
+
+
+case object TestLine {
+  def testLineOp(op: OPERATOR, splitString: Array[String], q_unsorted: List[(String, List[String])]): Boolean = {
+    val newString = splitString.filter(x => q_unsorted.map(y => y._1).contains(x.split("=")(0)))
+    if (q_unsorted.isEmpty) {
+      return false
+    }
+    op match {
+      case AND => testLineAnd(newString.sortBy(_.toLowerCase), q_unsorted.sortBy(_._1.toLowerCase), 0)
+      case OR => testLineOr(newString.sortBy(_.toLowerCase), q_unsorted.sortBy(_._1.toLowerCase), 0)
+      case _ => false
+    }
+  }
+
+  /**
+   * recursively checks if the provided string checks all the criteria of qV_sorted
+   * you can prefix >, <, >=, <= or ! (not equal) to compare more easily
+   * @param splitString string to test
+   * @param q_sorted    criteria, in form (field, list of acceptable values)
+   * @param n           index of field considered
+   * @return true <=> one of the condition is not fulfilled
+   */
+  private def testLineAnd(splitString: Array[String], q_sorted: List[(String, List[String])], n: Int): Boolean = {
+    if (n != splitString.length) {
+      if (q_sorted(n)._2.isEmpty) {
+        return testLineAnd(splitString, q_sorted, n + 1)
+      }
+      for (i <- q_sorted(n)._2.indices) {
+        q_sorted(n)._2(i) match {
+          case s if s.startsWith("!") =>
+            //when specified that the string must NOT equal a value
+            if (!splitString(n).contains(s.replaceFirst("!", ""))) {
+              return testLineAnd(splitString, q_sorted, n + 1)
+            }
+          case s if s.startsWith(">=") =>
+            //retrieve all the numbers in this string, and test if one of them matches the criterion
+            if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat >= s.replaceFirst(">=", "").toFloat)) {
+              return testLineAnd(splitString, q_sorted, n + 1)
+            }
+          case s if s.startsWith("<=") =>
+            //retrieve all the numbers in this string, and test if one of them matches the criterion
+            if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat <= s.replaceFirst("<=", "").toFloat)) {
+              return testLineAnd(splitString, q_sorted, n + 1)
+            }
+          case s if s.startsWith(">") =>
+            //retrieve all the numbers in this string, and test if one of them matches the criterion
+
+            if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat > s.replaceFirst(">", "").toFloat)) {
+              return testLineAnd(splitString, q_sorted, n + 1)
+            }
+
+          case s if s.startsWith("<") =>
+            //retrieve all the numbers in this string, and test if one of them matches the criterion
+
+            if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat < s.replaceFirst("<", "").toFloat)) {
+              return testLineAnd(splitString, q_sorted, n + 1)
+            }
+          case s =>
+            try {
+              val conv = s.toFloat
+              if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat == conv)) {
+                return testLineAnd(splitString, q_sorted, n + 1)
+              }
+            } catch {
+              case e : Exception =>
+                if (splitString(n).contains(s)) {
+                  return testLineAnd(splitString, q_sorted, n + 1)
+                }
+            }
+        }
+      }
+      true
+
+    } else {
+      false
+    }
+  }
+
+  /**
+   * recursively checks if the provided string checks one of the criteria of qV_sorted
+   * you can prefix >, <, >=, <= or ! (not equal) to compare more easily
+   * @param splitString string to test
+   * @param q_sorted    criteria, in form (field, list of acceptable values)
+   * @param n           index of field considered
+   * @return true <=> all the conditions are not fulfilled
+   */
+  private def testLineOr(splitString: Array[String], q_sorted: List[(String, List[String])], n: Int): Boolean = {
+    if (q_sorted.flatMap(x => x._2).isEmpty) {
+      return false
+    }
+    if (n != splitString.length) {
+      if (q_sorted(n)._2.isEmpty) {
+        return testLineOr(splitString, q_sorted, n + 1)
+      }
+      for (i <- q_sorted(n)._2.indices) {
+        q_sorted(n)._2(i) match {
+          case s if s.startsWith("!") =>
+            //when specified that the string must NOT equal a value
+            if (!splitString(n).contains(s.replaceFirst("!", ""))) {
+              return false
+            }
+          case s if s.startsWith(">=") =>
+            //retrieve all the numbers in this string, and test if one of them matches the criterion
+            if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat >= s.replaceFirst(">=", "").toFloat)) {
+              return false
+            }
+          case s if s.startsWith("<=") =>
+            //retrieve all the numbers in this string, and test if one of them matches the criterion
+            if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat <= s.replaceFirst("<=", "").toFloat)) {
+              return false
+            }
+          case s if s.startsWith(">") =>
+            //retrieve all the numbers in this string, and test if one of them matches the criterion
+            if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat > s.replaceFirst(">", "").toFloat)) {
+              return false
+            }
+          case s if s.startsWith("<") =>
+            //retrieve all the numbers in this string, and test if one of them matches the criterion
+            if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat < s.replaceFirst("<", "").toFloat)) {
+              return false
+            }
+          case s =>
+            try {
+              val conv = s.toFloat
+              if (("""\d+""".r findAllIn splitString(n)).toList.exists(string => string.toFloat == conv)) {
+                return false
+              }
+            } catch {
+              case e : Exception =>
+                if (splitString(n).contains(q_sorted(n)._2(i))) {
+                  return false
+                }
+            }
+        }
+      }
+      testLineOr(splitString, q_sorted, n + 1)
+
+    } else {
+      true
+    }
+  }
+}
