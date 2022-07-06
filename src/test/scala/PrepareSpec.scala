da831ae6a99019ab13b69312f3e36fc603e37951
EloiEynard <eloi.eynard@hotmail.com>
2022-07-06 17:42:09 +0200

    corrected SetTrieBatchPrepareWithInt implementation

--- src/test/scala/PrepareSpec.scala
+++ src/test/scala/PrepareSpec.scala
@@ -1,3 +1,4 @@
+import core.materialization.builder._
 import core.materialization._
 import core.prepare._
 import frontend.experiments.Tools
@@ -7,20 +8,16 @@ import util.Profiler
 
 class PrepareSpec extends FlatSpec with Matchers {
 
-  def isSameAs(oldpmd: Seq[ProjectionMetaData], newpmd: Seq[ProjectionMetaData]) = {
-    assert(oldpmd.length == newpmd.length)
-    oldpmd.zip(newpmd).foreach{case (o, n) => assert(o.accessible_bits.sameElements(n.accessible_bits) && o.mask.length == n.mask.length)}
-  }
   def RMS(nbits: Int, dmin: Int, logncubs: Int, nq: Int, qs: Int, cheap: Int, maxFetch: Int): Unit = {
     val m = RandomizedMaterializationScheme2(nbits, logncubs, dmin, dmin + logncubs)
     (0 until nq).foreach{ i =>
       val q = Tools.rand_q(nbits, qs)
       val oldpo = Profiler("OldPrepareOnline"){ClassicPreparer.prepareOnline(m, q, cheap, maxFetch)}.map(p => ProjectionMetaData(p.accessible_bits, p.accessible_bits0.toList.sorted, p.mask, p.id)).sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       val newpo = Profiler("NewPrepareOnline"){Preparer.default.prepareOnline(m, q, cheap, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
-      isSameAs(oldpo, newpo)
-      val oldpb = Profiler("OldPrepareBatch"){ClassicPreparer.prepareBatch(m, q, maxFetch)}.map(p => ProjectionMetaData(p.accessible_bits, p.accessible_bits0.toList.sorted, p.mask, p.id)).sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
-      val newpb = Profiler("NewPrepareBatch"){Preparer.default.prepareBatch(m, q, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
-      isSameAs(oldpb, newpb)
+      assert(oldpo.sameElements(newpo))
+      val oldpb = Profiler("OldPrepareOnline"){ClassicPreparer.prepareBatch(m, q, maxFetch)}.map(p => ProjectionMetaData(p.accessible_bits, p.accessible_bits0.toList.sorted, p.mask, p.id)).sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
+      val newpb = Profiler("NewPrepareOnline"){Preparer.default.prepareBatch(m, q, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
+      assert(oldpb.sameElements(newpb))
     }
     println("Time for RMS")
     Profiler.print()
@@ -35,8 +32,8 @@ class PrepareSpec extends FlatSpec with Matchers {
       val oldpo = Profiler("OldPrepareOnline"){ClassicPreparer.prepareOnline(m, q, cheap, maxFetch)}.map(p => ProjectionMetaData(p.accessible_bits, p.accessible_bits0.toList.sorted, p.mask, p.id)).sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       val newpo = Profiler("NewPrepareOnline"){SetTrieOnlinePrepareNoInt.prepareOnline(m, q, cheap, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       val newpo_int = Profiler("NewPrepareOnlineWithInt"){SetTrieOnlinePrepareWithInt.prepareOnline(m, q, cheap, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
-      isSameAs(oldpo, newpo)
-      isSameAs(newpo, newpo_int)
+      assert(oldpo.sameElements(newpo))
+      assert(oldpo.sameElements(newpo_int))
     }
     println("\nTime for RMS")
     Profiler.print()
@@ -50,7 +47,7 @@ class PrepareSpec extends FlatSpec with Matchers {
       print(i + " ")
       val oldpb = Profiler("OldPrepareBatch"){SetTrieBatchPrepare1.prepareBatch(m, q, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       val newpb = Profiler("NewPrepareBatchWithInt"){SetTrieBatchPrepareWithInt.prepareBatch(m, q, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
-      isSameAs(oldpb, newpb)
+      assert(oldpb.sameElements(newpb))
     }
     println("\nTime for RMS")
     Profiler.print()
@@ -65,8 +62,8 @@ class PrepareSpec extends FlatSpec with Matchers {
       val onlinepo_int = Profiler("NewPrepareOnlineWithInt"){SetTrieOnlinePrepareWithInt.prepareOnline(m, q, cheap, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       val oldpb = Profiler("OldPrepareBatch"){SetTrieBatchPrepare1.prepareBatch(m, q, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
       val newpb = Profiler("NewPrepareBatchWithInt"){SetTrieBatchPrepareWithInt.prepareBatch(m, q, maxFetch)}.sortBy(p => p.accessible_bits.mkString("") + "_" +p.mask.length + "_" + p.id)
-      isSameAs(oldpo, onlinepo_int)
-      isSameAs(oldpb, newpb)
+      assert(oldpo.sameElements(onlinepo_int))
+      assert(oldpb.sameElements(newpb))
     }
     println("\nTime for RMS")
     Profiler.print()
