244952b5346ee36da5b759058996725048d401d2
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2022-07-27 10:46:56 +0200

    Update first version of dropout IPF, add documentations

--- src/main/scala/core/solver/iterativeProportionalFittingSolver/DropoutEffectiveIPFSolver.scala
+++ src/main/scala/core/solver/iterativeProportionalFittingSolver/DropoutEffectiveIPFSolver.scala
@@ -1,13 +1,15 @@
 package core.solver.iterativeProportionalFittingSolver
 
-import core.solver.iterativeProportionalFittingSolver.IPFUtils.getNumOnesInBinary
+import core.SolverTools.entropy
+import core.solver.iterativeProportionalFittingSolver.IPFUtils.{getNumOnesInBinary, isVariablesContained}
 import util.{Bits, Profiler}
 
-import scala.util.Random
 import scala.util.control.Breaks.{break, breakable}
 
 /**
  * Variant of the effective IPF where we simply throw out some low-dimensional cuboids.
+ * TODO: Maybe a generalization for the current two heuristics of dropout.
+ * @author Zhekai Jiang
  * @param querySize Total number of dimensions queried.
  */
 class DropoutEffectiveIPFSolver(override val querySize: Int) extends EffectiveIPFSolver(querySize) {
@@ -29,7 +31,7 @@ class DropoutEffectiveIPFSolver(override val querySize: Int) extends EffectiveIP
    * @return totalDistribution as a one-dimensional array (values encoded as bits of 1 in index).
    */
   override def solve(): Array[Double] = {
-    dropOutLowDimensionalClusters()
+    selectiveDropout()
 
     clusters.foreach(cluster => graphicalModel.connectNodesCompletely(Bits.fromInt(cluster.variables).map(graphicalModel.nodes(_)).toSet, cluster))
 
@@ -61,28 +63,49 @@ class DropoutEffectiveIPFSolver(override val querySize: Int) extends EffectiveIP
     solution
   }
 
-  def dropOutLowDimensionalClusters(): Unit = {
+  /**
+   * Select clusters to drop out.
+   * At each time, the method will simply throw out one random cluster among those of the lowest dimension
+   * but will ignore a cluster if it contains at least one variable that is not covered by other remaining clusters.
+   * The dropout will stop when
+   * (1) 95% of the clusters have already been dropped,
+   * (2) we have reached the maximum dimensionality of the fetched cuboids (i.e. we will keep all cuboids at the maximum dimensionality),
+   * (3) the remaining "coverage", defined by the sum of the number of variables among all clusters, drops below |Q| * (|Q| - 1)
+   *     - this threshold can be adjusted—right now it simply comes from the fact that
+   *       if we have all possible (|Q|-1)-dimensional cuboids, this "coverage" will be (|Q|-1) * C(|Q|, |Q|-1) = |Q| * (|Q|-1)
+   * TODO: Some data structures to compute these things more efficiently
+   */
+  def selectiveDropout(): Unit = {
     var numDroppedClusters: Int = 0
     val totalNumClusters = clusters.length
     val maxNumDimensions = clusters.map(cluster => getNumOnesInBinary(cluster.variables)).max
-    val coverage: Int = clusters.map(_.numVariables).sum
-    var reducedCoverage: Int = 0
+    var coverage: Int = clusters.map(_.numVariables).sum
+    var currentNumDimensions: Int = clusters.map(cluster => getNumOnesInBinary(cluster.variables)).min
+    print("\t\t\tDropping out cuboids of sizes ")
     breakable { while (true) {
-      if (numDroppedClusters >= totalNumClusters * 0.3) {
+      if (numDroppedClusters >= totalNumClusters * 0.95) {
         break
       }
-      val minNumDimensions = clusters.map(cluster => getNumOnesInBinary(cluster.variables)).min
-      if (minNumDimensions * 2 > maxNumDimensions) {
+      if (currentNumDimensions >= maxNumDimensions) {
         break
       }
-      val droppedCluster = Random.shuffle(clusters.filter(cluster => getNumOnesInBinary(cluster.variables) == minNumDimensions)).head
-      numDroppedClusters += 1
-      reducedCoverage += droppedCluster.numVariables
-      if (reducedCoverage >= coverage * 0.3 || coverage - reducedCoverage < querySize * 3) {
-        break
+      val candidateClustersToDrop = clusters.filter(cluster =>
+        cluster.numVariables == currentNumDimensions
+          && Bits.fromInt(cluster.variables).forall(variable => (clusters.toSet - cluster).exists(cluster => isVariablesContained(1 << variable, cluster.variables)))
+      )
+      if (candidateClustersToDrop.isEmpty) {
+        currentNumDimensions += 1
+      } else {
+        val droppedCluster = candidateClustersToDrop.maxBy(cluster => entropy(cluster.distribution))
+        if (coverage - droppedCluster.numVariables < querySize * (querySize - 1)) {
+          break
+        }
+        coverage -= droppedCluster.numVariables
+        numDroppedClusters += 1
+        print(s"${Bits.fromInt(droppedCluster.variables).size}:")
+        clusters = clusters.filter(_ != droppedCluster)
       }
-      println(s"\t\t\tDropping out ${Bits.fromInt(droppedCluster.variables).mkString(":")}")
-      clusters = clusters.filter(_ != droppedCluster)
     } }
+    println(s", total of $numDroppedClusters cuboids")
   }
 }
