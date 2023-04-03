130ed54e218413bec6437c80651cd724f10305fe
Dixit Sabharwal <dixit.sabharwal@epfl.ch>
2023-04-03 14:38:16 +0200

    fix bug in SingleCuboidWaveletSolver

--- src/main/scala/core/solver/wavelet/SingleCuboidWaveletSolver.scala
+++ src/main/scala/core/solver/wavelet/SingleCuboidWaveletSolver.scala
@@ -30,13 +30,13 @@ class SingleCuboidWaveletSolver(val querysize: Int) extends WaveletSolver("Singl
     var result: Array[Double] = null
     Profiler(s"$solverName Permutation") {
       // permute cuboid to the correct position
-      val sourceOrder = variables ++ (0 until N).filterNot(i => variables.contains(i))
+      val sourceOrder = variables ++ (0 until querysize).filterNot(i => variables.contains(i))
 
       // target order is descending bit positions
       val targetOrder = sourceOrder.sorted(Ordering[Int].reverse)
 
-      val sourceIndex = BitUtils.permute_bits(N, sourceOrder.toArray)
-      val targetIndex = BitUtils.permute_bits(N, targetOrder.toArray)
+      val sourceIndex = BitUtils.permute_bits(querysize, sourceOrder.toArray)
+      val targetIndex = BitUtils.permute_bits(querysize, targetOrder.toArray)
 
       // map values at sourceIndex in extrapolatedCuboid to targetIndex in result
       result = Array.fill(N)(0.0)
