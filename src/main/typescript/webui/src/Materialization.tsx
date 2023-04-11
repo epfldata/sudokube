28e54f331dceba13f9fa4f6ca8d3b9d8eae5181e
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2023-04-11 21:35:40 +0200

    WebUI: Add mockup for materialization view

--- src/main/typescript/webui/src/Materialization.tsx
+++ src/main/typescript/webui/src/Materialization.tsx
@@ -1,7 +1,84 @@
 import * as React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import { FilterChip, chipStyle } from './Chips';
+import Chip from '@mui/material/Chip';
+import FilterAltIcon from '@mui/icons-material/FilterAlt';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Box } from '@mui/material';
 
 export function Materialization() {
+  const columns: GridColDef[] = [
+    {
+      field: 'country',
+      headerName: 'Country (6 bits)',
+      flex: 1,
+      sortable: false,
+      disableColumnMenu: true
+    },
+    {
+      field: 'city',
+      headerName: 'City (6 bits)',
+      flex: 1,
+      sortable: false,
+      disableColumnMenu: true
+    },
+    {
+      field: 'year',
+      headerName: 'Year (6 bits)',
+      flex: 1,
+      sortable: false,
+      disableColumnMenu: true
+    },
+    {
+      field: 'month',
+      headerName: 'Month (5 bits)',
+      flex: 1,
+      sortable: false,
+      disableColumnMenu: true
+    },
+    {
+      field: 'day',
+      headerName: 'Day (6 bits)',
+      flex: 1,
+      sortable: false,
+      disableColumnMenu: true
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      country: '\u2589\u2589\u2589\u25A2\u25A2\u25A2',
+      city: '\u25A2\u25A2\u25A2\u25A2\u25A2\u25A2',
+      year: '\u25A2\u25A2\u25A2\u25A2\u25A2\u25A2',
+      month: '\u2589\u2589\u2589\u2589\u25A2',
+      day: '\u25A2\u25A2\u25A2\u25A2\u25A2\u25A2'
+    }
+  ];
   return (
-    <h1>Materialization</h1>
+    <Container style={{ paddingTop: '20px' }}>
+      <Grid container maxHeight='30vh' overflow='scroll' style={{ paddingTop: '1px', paddingBottom: '1px' }}>
+        <Grid item xs={6}>
+          <FilterChip text='Country / 5–5' onDelete={() => {}} />
+          <Chip
+            icon = {<FilterAltIcon style = {{ height: '18px' }} />}
+            label = 'Add ...'
+            onClick = {() => {}}
+            style = {chipStyle}
+            variant = 'outlined'
+            color = 'primary'
+          />
+        </Grid>
+      </Grid>
+      <Box sx = {{ height: '80vh', width: '100%', marginTop: '20px' }}>
+        <DataGrid
+          rows = {rows}
+          columns = {columns}
+          checkboxSelection
+          disableRowSelectionOnClick
+        />
+      </Box>
+    </Container>
   )
 }
