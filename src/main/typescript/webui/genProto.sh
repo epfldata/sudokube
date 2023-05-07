e15d786b4fd751aa63eadadffa7aa45d647bd9ec
Zhekai Jiang <zhekai.jiang@mail.mcgill.ca>
2023-05-07 15:33:32 +0200

    WebUI: Add protoc dependency, modify path to protoc in genProto.sh

--- src/main/typescript/webui/genProto.sh
+++ src/main/typescript/webui/genProto.sh
@@ -1,6 +1,6 @@
 OUTDIR=src/_proto
 mkdir -p ${OUTDIR}
-protoc \
+./node_modules/protoc/bin/protoc \
 	--proto_path=protos \
 	--plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
 	--plugin=protoc-gen-js=./node_modules/.bin/protoc-gen-js \
