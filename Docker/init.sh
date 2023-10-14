a5d1e107d65518a89b11464a282fe2a7ecaf2720
sachinbjohn <sachinbjohn@gmail.com>
2023-10-14 11:59:53 +0200

    Update init.sh scripts

--- Docker/init.sh
+++ Docker/init.sh
@@ -37,8 +37,8 @@ cd /var/data/sudokube/sudokube
 mkdir tabledata expdata cubedata
 mount /dev/sdb cubedata
 mount /dev/sdc tabledata
-rclone copy skRW:12327-755e92f906ff1f273f45a3a76785476f/colstoreMay23  cubedata -P  --multi-thread-streams=8 --transfers=16
-rclone copy --include "*.uniq" skRW:12327-755e92f906ff1f273f45a3a76785476f/tabledata tabledata   -P --multi-thread-streams=8 --transfers=16
+rclone copy skRW:12327-755e92f906ff1f273f45a3a76785476f/colstoreMay23  cubedata -P  --multi-thread-streams=16 --transfers=16 --s3-chunk-size 64M --checkers=16
+rclone copy --include "*.uniq" skRW:12327-755e92f906ff1f273f45a3a76785476f/tabledata tabledata   -P --multi-thread-streams=16 --transfers=16
 #sync
 #sbt compile
 #make shared lib !
